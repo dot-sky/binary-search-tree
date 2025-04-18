9948bad6c422cb691ab0fe847c6a4a794f8a8e67
dot-sky <108539845+dot-sky@users.noreply.github.com>
2025-04-18 08:46:09 -0400

    Create traverse and rebalance methods

--- Tree.js
+++ Tree.js
@@ -26,7 +26,6 @@ export class Tree {
     if (start > end) return null;
     const mid = Math.floor((start + end) / 2);
     const node = new Node(arr[mid]);
-    console.log(arr[mid], start, end);
     node.left = this.buildFromArray(arr, start, mid - 1);
     node.right = this.buildFromArray(arr, mid + 1, end);
 
@@ -84,7 +83,166 @@ export class Tree {
     }
   }
 
-  prettyPrint(node, prefix = "", isLeft = true) {
+  find(value) {
+    return this.findIn(this.root, value);
+  }
+
+  findIn(node, value) {
+    if (node === null) return null;
+    if (value < node.data) return this.findIn(node.left, value);
+    if (value > node.data) return this.findIn(node.right, value);
+    return node;
+  }
+
+  levelOrder(callback) {
+    if (callback instanceof Function) {
+      const queue = [];
+      if (!this.empty()) queue.push(this.root);
+      let node;
+      while (queue.length > 0) {
+        node = queue[0];
+        callback(node);
+        if (node.left) queue.push(node.left);
+        if (node.right) queue.push(node.right);
+        queue.shift();
+      }
+    } else {
+      throw new Error("Callback is needed to execute a level order traversal.");
+    }
+  }
+
+  levelOrderRecursive(callback) {
+    if (callback instanceof Function) {
+      const list = !this.empty() ? [this.root] : [];
+      this.#levelOrderRecursiveList(callback, list);
+    } else {
+      throw new Error("Callback is needed to execute a level order traversal.");
+    }
+  }
+
+  #levelOrderRecursiveList(callback, list) {
+    if (list.length === 0) return;
+    const children = [];
+    for (let node of list) {
+      callback(node);
+      if (node.left) children.push(node.left);
+      if (node.right) children.push(node.right);
+    }
+    this.#levelOrderRecursiveList(callback, children);
+  }
+
+  inOrder(callback) {
+    if (callback instanceof Function) {
+      this.inOrderIn(this.root, callback);
+    } else {
+      throw new Error("Callback is needed to execute a level order traversal.");
+    }
+  }
+
+  inOrderIn(node, callback) {
+    if (node === null) return;
+    this.inOrderIn(node.left, callback);
+    callback(node);
+    this.inOrderIn(node.right, callback);
+  }
+
+  preOrder(callback) {
+    if (callback instanceof Function) {
+      this.preOrderIn(this.root, callback);
+    } else {
+      throw new Error("Callback is needed to execute a level order traversal.");
+    }
+  }
+
+  preOrderIn(node, callback) {
+    if (node === null) return;
+    callback(node);
+    this.preOrderIn(node.left, callback);
+    this.preOrderIn(node.right, callback);
+  }
+
+  postOrder(callback) {
+    if (callback instanceof Function) {
+      this.postOrderIn(this.root, callback);
+    } else {
+      throw new Error("Callback is needed to execute a level order traversal.");
+    }
+  }
+
+  postOrderIn(node, callback) {
+    if (node === null) return;
+    this.postOrderIn(node.left, callback);
+    this.postOrderIn(node.right, callback);
+    callback(node);
+  }
+
+  height(value, node = this.root) {
+    if (node === null) return null;
+
+    let height = null;
+
+    if (value === node.data) return 0;
+    else if (value < node.data) height = this.height(value, node.left);
+    else if (value > node.data) height = this.height(value, node.right);
+
+    if (height !== null) return height + 1;
+
+    return null;
+  }
+
+  isBalanced(node = this.root) {
+    if (node === null) return true;
+
+    const diff = Math.abs(
+      this.treeHeight(node.left) - this.treeHeight(node.right)
+    );
+
+    if (diff <= 1)
+      return this.isBalanced(node.left) && this.isBalanced(node.right);
+    console.log(node.data);
+    return false;
+  }
+
+  rebalance() {
+    const nodes = [];
+    const queue = [];
+    if (!this.empty()) {
+      queue.push(this.root);
+      nodes.push(this.root.data);
+    }
+    let node;
+    while (queue.length > 0) {
+      node = queue[0];
+      nodes.push(node.data);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+      queue.shift();
+    }
+    this.root = this.buildTree(nodes);
+  }
+
+  empty() {
+    return this.root === null;
+  }
+
+  treeHeight(node) {
+    if (node === null) return -1;
+
+    const leftHeight = this.treeHeight(node.left);
+    const rightHeight = this.treeHeight(node.right);
+
+    return Math.max(leftHeight, rightHeight) + 1;
+  }
+
+  fillRandom(min, max, quantity = 20) {
+    const arr = [];
+    for (let i = 0; i < quantity; i++) {
+      arr.push(Math.floor(Math.random() * (max - min))) + min;
+    }
+    this.root = this.buildTree(arr);
+  }
+
+  prettyPrint(node = this.root, prefix = "", isLeft = true) {
     if (node === null) {
       return;
     }
--- main.js
+++ main.js
@@ -1,35 +1,52 @@
 import { Tree } from "./Tree.js";
-const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+function printData(elem) {
+  console.log(elem.data);
+}
 
-tree.insert(17);
-tree.insert(0);
-tree.insert(18);
-tree.insert(16);
-tree.insert(325);
-tree.insert(327);
+const tree = new Tree();
+tree.fillRandom(0, 100, 20);
 tree.prettyPrint(tree.root);
-console.log("==========================================");
-tree.deleteItem(4);
-tree.prettyPrint(tree.root);
-console.log("==========================================");
-tree.deleteItem(67);
-tree.prettyPrint(tree.root);
-console.log("==========================================");
-tree.deleteItem(8);
-tree.prettyPrint(tree.root);
-console.log("==========================================");
-tree.deleteItem(9);
-tree.prettyPrint(tree.root);
-console.log("==========================================");
-tree.deleteItem(23);
-tree.prettyPrint(tree.root);
-console.log("==========================================");
-tree.deleteItem(5);
-tree.prettyPrint(tree.root);
-console.log("==========================================");
-tree.deleteItem(324);
-tree.prettyPrint(tree.root);
-console.log("==========================================");
-tree.deleteItem(6345);
-tree.prettyPrint(tree.root);
-console.log("==========================================");
+
+console.log({ isBalanced: tree.isBalanced() });
+console.log("Level Order");
+tree.levelOrder(printData);
+
+console.log("Inorder");
+tree.inOrder(printData);
+
+console.log("Preorder");
+tree.preOrder(printData);
+
+console.log("Postorder");
+tree.postOrder(printData);
+
+// Unbalancing tree
+
+tree.insert(100);
+tree.insert(118);
+tree.insert(104);
+tree.insert(124);
+tree.insert(103);
+tree.insert(103);
+tree.insert(130);
+
+tree.prettyPrint();
+console.log({ isBalanced: tree.isBalanced() });
+
+// Rebalancing tree
+
+tree.rebalance();
+tree.prettyPrint();
+console.log({ isBalanced: tree.isBalanced() });
+
+console.log("Level Order");
+tree.levelOrder(printData);
+
+console.log("Inorder");
+tree.inOrder(printData);
+
+console.log("Preorder");
+tree.preOrder(printData);
+
+console.log("Postorder");
+tree.postOrder(printData);
